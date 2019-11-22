e67c45b2a04dbe09a14e394c6f0126ca97620f0a
alcim <alcim@DESKTOP-KI6UDBO>
2019-11-22 19:36:37 -0300

    Commit 4

--- AulaThiago/src/exercicios/Hotel.java
+++ AulaThiago/src/exercicios/Hotel.java
@@ -1,19 +1,28 @@
+import java.util.ArrayList;
 import java.util.Collection;
 
 public class Hotel {
+	private Collection<Comodiade> comodidade;
+	
+	public Hotel(Collection<Comodidade> comodidade) {
+		super();
+		this.comodidade = comodidade;
+	}
 
-	private int id;
-
-	private String nome;
-
-	private String endereco;
-
-	private String cidade;
-
-	private int classe;
-
-	private String telefone;
-
-	private Collection comodidades;
-
+		public Collection<Comodidade> getComodidade() {
+			return comodidade;
+		}
+		public String toString() {
+			return "Hotel [comodidade=" + comodidade + "]";
+		}
+	
+	public static void main(String[] args) {
+		Collection<Comodidade> comodidades = new ArrayList<Comodidade>();
+		comodidades.add(Comodidade.ACADEMIA);
+		comodidades.add(Comodidade.ESTACIONAMENTO);
+		Hotel hotel = new Hotel(comodidades);
+		
+		System.out.println(hotel);
+		
+	}
 }
--- AulaThiago/src/exercicios/Item.java
+++ AulaThiago/src/exercicios/Item.java
@@ -1,5 +1,13 @@
 public class Item {
 
+	public String getItem() {
+		return item;
+	}
+
+	public void setItem(String item) {
+		this.item = item;
+	}
+
 	private int ID;
 
 	private String item;
